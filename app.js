8f12e471553c4306acdefad40650b68f2de0585d
Brumus14 <luke.barkess@icloud.com>
2023-02-18 01:19:59 +0000

    Dots and cleaned up cursor

--- app.js
+++ app.js
@@ -1,10 +1,51 @@
-elements = {
-  cursor: document.querySelector("#cursor"),
-};
-
+//#region Cursor Control
 document.onmousemove = (event) => {
-  elements.cursor.style.left = event.clientX + "px";
-  elements.cursor.style.top = event.clientY + "px";
-  console.log(`translate(${event.clientX - 1920 / 2},${event.clientY - 1080 / 2})`);
-  //document.querySelector("p").style.transform = `translate(${(event.clientX - 1920 / 2) / 64}px,${(event.clientY - 1080 / 2) / 64})`;
+  const cursor = document.querySelector("#cursor");
+
+  cursor.style.left = event.clientX + "px";
+  cursor.style.top = event.clientY + "px";
 };
+//#endregion
+
+//#region Canvas Background
+{
+  let heroTextBounds = document.querySelector("p").getBoundingClientRect();
+  let heroTextPadding = 100;
+  let windowPadding = 50;
+
+  const canvas = document.querySelector("#background-canvas");
+  const context = canvas.getContext("2d");
+  // Depending on distance from top left decrease dot size
+  // When scrolling dots move towards top left and change size
+  // When link clicked black dot at link which expands
+  // Splash effect on click
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+  function drawDot(x, y) {
+    let distance = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+    let dotSize = 50 / (1 + Math.pow(Math.E, -((-0.004 * distance) + 4)));
+
+    context.beginPath();
+    context.arc(x, y, dotSize, 0, 2 * Math.PI, false);
+    context.fillStyle = "lightblue";
+    context.fill();
+  }
+
+  for (let i = 0; i < 20; i++) {
+    dotPosition = {
+      x: randomNumber(windowPadding, window.innerWidth - windowPadding),
+      y: randomNumber(windowPadding, window.innerHeight - windowPadding)
+    };
+
+
+    if (dotPosition.x < heroTextBounds.left - heroTextPadding || dotPosition.x > heroTextBounds.right + heroTextPadding || dotPosition.y < heroTextBounds.top - heroTextPadding || dotPosition.y > heroTextBounds.bottom + heroTextPadding) {
+      drawDot(dotPosition.x, dotPosition.y);
+    }
+  }
+}
+
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+//#endregion
