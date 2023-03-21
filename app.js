22bbdf601da2b22f952ae3d194db8cfe6827fca0
Brumus14 <luke.barkess@icloud.com>
2023-03-21 16:56:51 +0000

    circul

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+// Variables {{{
 let dotPositions = [];
 let cursorX = 0;
 let cursorY = 0;
+// }}}
 
-//#region Cursor Control
+// Cursor Control {{{
 {
   const cursor = document.querySelector("#cursor");
   let firstCursorMove = true;
@@ -21,9 +23,12 @@ let cursorY = 0;
     cursor.style.top = cursorY + "px";
   };
 
-  document.onmousedown = () => {
-    expandingDot();
-  }
+  // document.onmousedown = () => {
+  //   if (!firstCursorMove)
+  //   {
+  //     expandingDot();
+  //   }
+  // }
 
   document.querySelectorAll("button").forEach(button => {
     button.addEventListener("mouseenter", () => {
@@ -35,53 +40,18 @@ let cursorY = 0;
     });
   })
 }
-//#endregion
+// }}}
 
-//#region Canvas Background
+// Background {{{
 {
-  let heroTextBounds = document.querySelector("p").getBoundingClientRect();
-  let heroTextPadding = 100;
-  let windowPadding = 50;
-  let dotPadding = 300;
-
-  const canvas = document.querySelector("#background-canvas");
-  const context = canvas.getContext("2d");
-  // Depending on distance from top left decrease dot size
-  // When scrolling dots move towards top left and change size
-  // When link clicked black dot at link which expands
-  // Splash effect on click
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-
-  function drawDot(x, y) {
-    let distance = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
-    let dotSize = Math.max(50 / (1 + Math.pow(Math.E, -((-0.005 * distance) + 4))), 5);
-
-    context.beginPath();
-    context.arc(x, y, dotSize, 0, 2 * Math.PI, false);
-    context.fillStyle = "white";
-    context.fill();
-  }
-
-  function GenerateDot() {
-    dotPosition = {
-      x: randomNumber(windowPadding, window.innerWidth - windowPadding),
-      y: randomNumber(windowPadding, window.innerHeight - windowPadding)
-    };
-
-    let positionFree = true;
-
-    dotPositions.forEach(position => {
-      if (Math.sqrt(Math.pow(position.x - dotPosition.x, 2) + Math.pow(position.y - dotPosition.y, 2)) < dotPadding) {
-        positionFree = false;
-      }
-    });
-
-    if (positionFree && (dotPosition.x < heroTextBounds.left - heroTextPadding || dotPosition.x > heroTextBounds.right + heroTextPadding || dotPosition.y < heroTextBounds.top - heroTextPadding || dotPosition.y > heroTextBounds.bottom + heroTextPadding)) {
-      drawDot(dotPosition.x, dotPosition.y);
-      dotPositions.push(dotPosition);
-    }
-  }
+  // const canvas = document.querySelector("#background-canvas");
+  // const context = canvas.getContext("2d");
+  // // Depending on distance from top left decrease dot size
+  // // When scrolling dots move towards top left and change size
+  // // When link clicked black dot at link which expands
+  // // Splash effect on click
+  // canvas.width = window.innerWidth;
+  // canvas.height = window.innerHeight;
 
   function expandingDot() {
     let expandingCircle = document.createElement("div");
@@ -102,13 +72,9 @@ let cursorY = 0;
       expandingCircle.style.transform = "translate(-50%, -50%) scale(1)";
     }, 0);
   }
-
-  for (let i = 0; i < 20; i++) {
-    GenerateDot();
-  }
 }
 
 function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
-//#endregion
+// }}}
