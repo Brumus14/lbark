530a97ac6c575507deb0f5e4ca172b4b1e839f18
17BarkeL <luke.barkess@icloud.com>
2024-01-31 23:44:14 +0000

    More mobile responsiveness.

--- js/globals.js
+++ js/globals.js
@@ -5,6 +5,7 @@ let pointerCursorSize = "4rem";
 
 function globalOnLoad() {
   let fadingScreen = document.querySelector("#fading-screen");
+
   setTimeout(() => { fadingScreen.style.opacity = "0"; document.body.style.overflow = "hidden"; window.scrollTo(0, 0) }, 10);
   setTimeout(() => { fadingScreen.remove(); document.body.removeAttribute("style"); }, 310);
 
@@ -14,9 +15,27 @@ function globalOnLoad() {
     if (item.innerText.length > longest) {
       longest = item.innerText.length;
     }
+
+    if (window.innerWidth > 1500) {
+      item.style.transform = "translateY(0%)";
+    }
   });
 
   document.documentElement.style.setProperty("--menuItemWidth", (longest + 1) + "ch");
+
+  navToggleElement = document.querySelector(".nav-toggle");
+
+  navToggleElement.addEventListener("click", (event) => {
+    if (navToggleElement.classList.contains("active")) {
+      circleShrink();
+    }
+
+    else {
+      circleEnlarge(event.pageX - 50, event.pageY - 50);
+    }
+
+    navToggleElement.classList.toggle("active");
+  });
 }
 
 // Cursor Control
@@ -39,30 +58,55 @@ function globalOnLoad() {
     cursor.style.top = cursorY + "px";
   }
 
-  hoverableElement(document.querySelectorAll("a, button, .projects-container .container"));
+  hoverableElement(document.querySelectorAll("a, button, .projects-container .container, .nav-toggle"));
 }
 
 document.querySelectorAll(".nav-list a").forEach(element => {
   element.addEventListener("click", (event) => {
     event.preventDefault();
 
-    let circle = document.createElement("div");
-    circle.setAttribute("id", "circle-screen");
-    document.body.appendChild(circle);
-    circle.style.left = `${event.pageX - 50}px`;
-    circle.style.top = `${event.pageY - 50}px`;
+    circleEnlarge(event.pageX - 50, event.pageY - 50);
+    document.querySelector("#cursor").style.opacity = "0";
 
-    setTimeout(() => { circle.style.transform = "scale(50)"; document.querySelector("#cursor").style.opacity = "0"; document.body.style.overflow = "hidden"; }, 10);
     setTimeout(() => { document.location.href = event.target.getAttribute("href"); }, 710);
   });
 });
 
+function circleEnlarge(positionX, positionY) {
+  let circle = document.querySelector("#circle-screen");
+
+  if (circle == null) {
+    circle = document.createElement("div");
+    circle.setAttribute("id", "circle-screen");
+    document.body.appendChild(circle);
+  }
+
+
+  circle.style.left = `${positionX}px`;
+  circle.style.top = `${positionY}px`;
+
+  setTimeout(() => {
+    circle.style.transform = "scale(42)";
+    document.body.style.overflow = "hidden";
+  }, 10);
+}
+
+function circleShrink() {
+  console.log("sdf");
+  let circle = document.querySelector("#circle-screen");
+
+  circle.style.transform = "";
+  document.body.style.overflow = "";
+}
+
 function hoverableElement(elements) {
   elements.forEach(element => {
+    // When mouse enters make cursor larger
     element.addEventListener("mouseenter", () => {
       cursor.style.width = pointerCursorSize;
     });
     
+    // When mouse leaves
     element.addEventListener("mouseleave", () => {
       cursor.style.width = "";
     });
