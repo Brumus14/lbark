16c7d0ed15ea74978cb1a8e7c8d1cfd71f926c84
Luke Barkess <luke.barkess@icloud.com>
2025-10-21 00:42:51 +0100

    Fixed lots of bugs and updated responsiveness

--- js/globals.js
+++ js/globals.js
@@ -4,117 +4,135 @@ let cursorY = 0;
 let pointerCursorSize = "4rem";
 
 function globalOnLoad() {
-  let fadingScreen = document.querySelector("#fading-screen");
+    let fadingScreen = document.querySelector("#fading-screen");
 
-  setTimeout(() => { fadingScreen.style.opacity = "0"; document.body.style.overflow = "hidden"; window.scrollTo(0, 0) }, 10);
-  setTimeout(() => { fadingScreen.remove(); document.body.removeAttribute("style"); }, 310);
+    setTimeout(() => {
+        fadingScreen.style.opacity = "0";
+        document.body.style.overflow = "hidden";
+        window.scrollTo(0, 0);
+    }, 10);
+    setTimeout(() => {
+        fadingScreen.remove();
+        document.body.removeAttribute("style");
+    }, 310);
 
-  longest = 0;
+    longest = 0;
 
-  document.querySelectorAll(".nav-list a").forEach(item => {
-    if (item.innerText.length > longest) {
-      longest = item.innerText.length;
-    }
+    document.querySelectorAll(".nav-list a").forEach((item) => {
+        if (item.innerText.length > longest) {
+            longest = item.innerText.length;
+        }
 
-    if (window.innerWidth > 1500) {
-      item.style.transform = "translateY(0%)";
-    }
-  });
+        if (window.innerWidth > 1500) {
+            item.style.transform = "translateY(0%)";
+        }
+    });
 
-  document.documentElement.style.setProperty("--menuItemWidth", (longest + 1) + "ch");
+    document.documentElement.style.setProperty(
+        "--menuItemWidth",
+        longest + 1 + "ch",
+    );
 
-  navToggleElement = document.querySelector(".nav-toggle");
+    navToggleElement = document.querySelector(".nav-toggle");
 
-  navToggleElement.addEventListener("click", (event) => {
-    if (navToggleElement.classList.contains("active")) {
-      circleShrink();
-    }
+    navToggleElement.addEventListener("click", (event) => {
+        if (navToggleElement.classList.contains("active")) {
+            circleShrink();
+        } else {
+            circleEnlarge(event.pageX - 50, event.pageY - 50, false);
+        }
 
-    else {
-      circleEnlarge(event.pageX - 50, event.pageY - 50, false);
-    }
-
-    navToggleElement.classList.toggle("active");
-  });
+        navToggleElement.classList.toggle("active");
+    });
 }
 
 // Cursor Control
 {
-  const cursor = document.querySelector("#cursor");
-  let firstCursorMove = true;
+    const cursor = document.querySelector("#cursor");
+    let firstCursorMove = true;
 
-  document.addEventListener("mousemove", updateCursor);
+    document.addEventListener("mousemove", updateCursor);
 
-  function updateCursor(event) {
-    if (firstCursorMove) {
-      firstCursorMove = false;
-      cursor.style.opacity = 1;
+    function updateCursor(event) {
+        if (firstCursorMove) {
+            firstCursorMove = false;
+            cursor.style.opacity = 1;
+        }
+
+        cursorX = event.clientX;
+        cursorY = event.clientY;
+
+        cursor.style.left = cursorX + "px";
+        cursor.style.top = cursorY + "px";
     }
 
-    cursorX = event.clientX;
-    cursorY = event.clientY;
-
-    cursor.style.left = cursorX + "px";
-    cursor.style.top = cursorY + "px";
-  }
-
-  hoverableElement(document.querySelectorAll("a, button, .projects-container .container, .nav-toggle"));
+    hoverableElement(
+        document.querySelectorAll(
+            "a, button, .projects-container .container, .nav-toggle",
+        ),
+    );
 }
 
-document.querySelectorAll(".nav-list a").forEach(element => {
-  element.addEventListener("click", (event) => {
-    event.preventDefault();
+document.querySelectorAll(".nav-list a").forEach((element) => {
+    element.addEventListener("click", (event) => {
+        event.preventDefault();
 
-    circleEnlarge(event.pageX - 50, event.pageY - 50, true);
+        circleEnlarge(event.pageX - 50, event.pageY - 50, true);
 
-    let circles = document.querySelectorAll("#circle-screen");
-    circles[circles.length - 1].style.zIndex = 5;
-    document.querySelector("#cursor").style.opacity = "0";
+        let circles = document.querySelectorAll("#circle-screen");
+        circles[circles.length - 1].style.zIndex = 5;
+        document.querySelector("#cursor").style.opacity = "0";
 
-    setTimeout(() => { document.location.href = event.target.getAttribute("href"); }, 710);
-  });
+        setTimeout(() => {
+            document.location.href = event.target.getAttribute("href");
+        }, 710);
+    });
 });
 
 function circleEnlarge(positionX, positionY, newCircle) {
-  let circle = document.querySelector("#circle-screen");;
-  
-  if (newCircle || circle == null) {
-    circle = document.createElement("div");
-    circle.id = "circle-screen";
-    document.body.appendChild(circle);
-  }
+    let circle = document.querySelector("#circle-screen");
 
-  circle.style.left = `${positionX}px`;
-  circle.style.top = `${positionY}px`;
+    if (newCircle || circle == null) {
+        circle = document.createElement("div");
+        circle.id = "circle-screen";
+        document.body.appendChild(circle);
+    }
 
-  setTimeout(() => {
-    circle.style.transform = "scale(42)";
-    document.body.style.overflow = "hidden";
-  }, 10);
+    circle.style.left = `${positionX}px`;
+    circle.style.top = `${positionY}px`;
+
+    setTimeout(() => {
+        circle.style.transform = "scale(42)";
+        document.body.style.overflow = "hidden";
+        document.documentElement.style.overflow = "hidden";
+    }, 10);
 }
 
 function circleShrink() {
-  let circle = document.querySelector("#circle-screen");
+    let circle = document.querySelector("#circle-screen");
 
-  circle.style.transform = "";
+    circle.style.transform = "";
 
-  setTimeout(() => { document.body.style.overflow = ""; }, 710);
+    setTimeout(() => {
+        document.body.style.overflow = "";
+    }, 710);
 }
 
 function hoverableElement(elements) {
-  elements.forEach(element => {
-    // When mouse enters make cursor larger
-    element.addEventListener("mouseenter", () => {
-      cursor.style.width = pointerCursorSize;
+    elements.forEach((element) => {
+        // When mouse enters make cursor larger
+        element.addEventListener("mouseenter", () => {
+            cursor.style.width = pointerCursorSize;
+        });
+
+        // When mouse leaves
+        element.addEventListener("mouseleave", () => {
+            cursor.style.width = "";
+        });
     });
-    
-    // When mouse leaves
-    element.addEventListener("mouseleave", () => {
-      cursor.style.width = "";
-    });
-  });
 }
 
 function randomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
--- js/projects.js
+++ js/projects.js
@@ -1,177 +1,202 @@
 let defaultcolumnWidth = 680;
 let numberOfColumns = Math.ceil(
-	document.querySelector(".projects-container").getBoundingClientRect().width /
-		defaultcolumnWidth,
+    document.querySelector(".projects-container").getBoundingClientRect()
+        .width / defaultcolumnWidth,
 );
 
-// function loadProjects() {
-// 	let projectData = [
-// 		["Raycaster", "A basic raycasting program made in C++ with SFML"],
-// 	];
-//
-// 	for (let i = 0; i < numberOfColumns; i++) {
-// 		let newColumn = document.createElement("div");
-// 		newColumn.className = "column";
-// 		document.querySelector(".projects").appendChild(newColumn);
-// 	}
-//
-// 	let columns = document.querySelectorAll(".projects .column");
-//
-// 	projectData.forEach((project) => {
-// 		let newProject = document.createElement("div");
-// 		newProject.className = "container";
-// 		newProject.innerHTML = `
-//     <p class="title">${project[0]}</p>
-//     <p class="description">${project[1]}</p>`;
-// 		document.querySelector(".projects").appendChild(newProject);
-// 	});
-//
-// 	let projects = document.querySelectorAll(".projects .container");
-//
-// 	projects.forEach((project) => {
-// 		let smallestIndex = 0;
-//
-// 		for (let i = 1; i < columns.length; i++) {
-// 			if (
-// 				columns[i].getBoundingClientRect().height <
-// 				columns[smallestIndex].getBoundingClientRect().height
-// 			) {
-// 				smallestIndex = i;
-// 			}
-// 		}
-//
-// 		columns[smallestIndex].appendChild(project);
-// 	});
-//
-// 	hoverableElement(document.querySelectorAll(".projects .container"));
-// }
+// Name, Description, Website
+const webAppData = [
+    // ["Rollr", "An adjustable dice simulator", "rollr"],
+    // ["Stimer", "A timer", "stimer"],
+    // ["Genn", "Secure password generator", "genn"],
+    // ["Rring", "A simple alarm", "rring"],
+    ["VCtimer", "A Rubiks cube timer with stats", "vctimer"],
+    // ["Numer", "A basic easy to use calculator", "numer"],
+    // ["Klick", "A minimal cps tester", "klick"],
+];
+
+const repositories = [
+    "dotfiles",
+    "minecraft-clone",
+    "HTTP-server",
+    "physics-engine",
+    "Rust-MD5-hasher",
+    "SHA-256",
+    "lbark",
+    "LMCE",
+    "horrible-game",
+    "scripts",
+];
+
+function loadProjects() {
+    // 	let projectData = [
+    // 		["Raycaster", "A basic raycasting program made in C++ with SFML"],
+    // 	];
+    //
+    // 	for (let i = 0; i < numberOfColumns; i++) {
+    // 		let newColumn = document.createElement("div");
+    // 		newColumn.className = "column";
+    // 		document.querySelector(".projects").appendChild(newColumn);
+    // 	}
+    //
+    // 	let columns = document.querySelectorAll(".projects .column");
+    //
+    // 	projectData.forEach((project) => {
+    // 		let newProject = document.createElement("div");
+    // 		newProject.className = "container";
+    // 		newProject.innerHTML = `
+    //     <p class="title">${project[0]}</p>
+    //     <p class="description">${project[1]}</p>`;
+    // 		document.querySelector(".projects").appendChild(newProject);
+    // 	});
+    //
+    // 	let projects = document.querySelectorAll(".projects .container");
+    //
+    // 	projects.forEach((project) => {
+    // 		let smallestIndex = 0;
+    //
+    // 		for (let i = 1; i < columns.length; i++) {
+    // 			if (
+    // 				columns[i].getBoundingClientRect().height <
+    // 				columns[smallestIndex].getBoundingClientRect().height
+    // 			) {
+    // 				smallestIndex = i;
+    // 			}
+    // 		}
+    //
+    // 		columns[smallestIndex].appendChild(project);
+    // 	});
+    //
+    // 	hoverableElement(document.querySelectorAll(".projects .container"));
+}
 
 function loadWebApps() {
-	let webAppData = [
-		["Rollr", "An adjustable dice simulator", "rollr"],
-		["Stimer", "A timer", "stimer"],
-		["Genn", "Secure password generator", "genn"],
-		["Rring", "A simple alarm", "rring"],
-		["VCtimer", "A Rubiks cube timer with stats", "vctimer"],
-		["Numer", "A basic easy to use calculator", "numer"],
-		["Klick", "A minimal cps tester", "klick"],
-	];
+    for (let i = 0; i < numberOfColumns; i++) {
+        let newColumn = document.createElement("div");
+        newColumn.className = "column";
+        document.querySelector(".web-apps").appendChild(newColumn);
+    }
 
-	for (let i = 0; i < numberOfColumns; i++) {
-		let newColumn = document.createElement("div");
-		newColumn.className = "column";
-		document.querySelector(".web-apps").appendChild(newColumn);
-	}
+    let columns = document.querySelectorAll(".web-apps .column");
 
-	let columns = document.querySelectorAll(".web-apps .column");
-
-	webAppData.forEach((webApp) => {
-		let newWebApp = document.createElement("div");
-		newWebApp.className = "container";
-		newWebApp.innerHTML = `
+    webAppData.forEach((webApp) => {
+        let newWebApp = document.createElement("div");
+        newWebApp.className = "container";
+        newWebApp.innerHTML = `
     <p class="title">${webApp[0]}</p>
     <p class="description">${webApp[1]}</p>
     <img src="./img/appLogos/${webApp[0].toLowerCase().replace(/ /g, "-")}.png" alt="">`;
-		document.querySelector(".web-apps").appendChild(newWebApp);
+        document.querySelector(".web-apps").appendChild(newWebApp);
 
-		newWebApp.addEventListener("click", () => {
-			window.open(`https://${webApp[2]}.netlify.app`);
-		});
-	});
+        newWebApp.addEventListener("click", () => {
+            window.open(`https://${webApp[2]}.netlify.app`);
+        });
+    });
 
-	let webAppElements = document.querySelectorAll(".web-apps .container");
+    let webAppElements = document.querySelectorAll(".web-apps .container");
 
-	webAppElements.forEach((app) => {
-		let smallestIndex = 0;
+    webAppElements.forEach((app) => {
+        let smallestIndex = 0;
 
-		for (let i = 1; i < columns.length; i++) {
-			if (
-				columns[i].getBoundingClientRect().height <
-				columns[smallestIndex].getBoundingClientRect().height
-			) {
-				smallestIndex = i;
-			}
-		}
+        for (let i = 1; i < columns.length; i++) {
+            if (
+                columns[i].getBoundingClientRect().height <
+                columns[smallestIndex].getBoundingClientRect().height
+            ) {
+                smallestIndex = i;
+            }
+        }
 
-		columns[smallestIndex].appendChild(app);
-	});
+        columns[smallestIndex].appendChild(app);
+    });
 
-	hoverableElement(document.querySelectorAll(".web-apps .container"));
+    hoverableElement(document.querySelectorAll(".web-apps .container"));
 }
 
 async function loadRepositories() {
-	for (let i = 0; i < numberOfColumns; i++) {
-		let newColumn = document.createElement("div");
-		newColumn.className = "column";
-		document.querySelector(".repositories").appendChild(newColumn);
-	}
+    for (let i = 0; i < numberOfColumns; i++) {
+        let newColumn = document.createElement("div");
+        newColumn.className = "column";
+        document.querySelector(".repositories").appendChild(newColumn);
+    }
 
-	let columns = document.querySelectorAll(".repositories .column");
-	let repositories = await fetchRepositories();
+    let columns = document.querySelectorAll(".repositories .column");
+    let repositories = await fetchRepositories();
 
-	repositories.forEach((repository) => {
-		let smallestIndex = 0;
+    repositories.forEach((repository) => {
+        let smallestIndex = 0;
 
-		for (let i = 1; i < columns.length; i++) {
-			if (
-				columns[i].getBoundingClientRect().height <
-				columns[smallestIndex].getBoundingClientRect().height
-			) {
-				smallestIndex = i;
-			}
-		}
+        for (let i = 1; i < columns.length; i++) {
+            if (
+                columns[i].getBoundingClientRect().height <
+                columns[smallestIndex].getBoundingClientRect().height
+            ) {
+                smallestIndex = i;
+            }
+        }
 
-		let newContainer = document.createElement("div");
-		newContainer.className = "container";
-		newContainer.innerHTML = `<p class="title">${repository.name}</p>`;
+        let newContainer = document.createElement("div");
+        newContainer.className = "container";
+        newContainer.innerHTML = `<p class="title">${repository.name}</p>`;
 
-		if (repository.description != null) {
-			newContainer.innerHTML += `<p class=description>${repository.description}</p>`;
-		}
+        if (repository.description != null) {
+            newContainer.innerHTML += `<p class=description>${repository.description}</p>`;
+        }
 
-		columns[smallestIndex].appendChild(newContainer);
+        columns[smallestIndex].appendChild(newContainer);
 
-		newContainer.addEventListener("click", () => {
-			window.open(`${repository.html_url}`);
-		});
-	});
+        newContainer.addEventListener("click", () => {
+            window.open(`${repository.html_url}`);
+        });
+    });
 
-	hoverableElement(document.querySelectorAll(".repositories .container"));
+    hoverableElement(document.querySelectorAll(".repositories .container"));
 }
 
 async function fetchRepositories() {
-	const response = await fetch("https://api.github.com/users/Brumus14/repos");
-	const repositories = await response.json();
+    let response = await fetch(
+        "https://api.github.com/users/Brumus14/repos",
+    ).then((res) => res.json());
 
-	return repositories;
+    for (let i = 0; i < response.length; i++) {
+        // Fetch info from GitHub
+        if (!repositories.includes(response[i].name)) {
+            response.splice(i, 1);
+        }
+    }
+    console.log(response);
+
+    return response;
 }
 
+// Update project column layout
 async function updateProjects() {
-	if (
-		numberOfColumns !=
-		Math.ceil(
-			document.querySelector(".projects-container").getBoundingClientRect()
-				.width / defaultcolumnWidth,
-		)
-	) {
-		numberOfColumns = Math.ceil(
-			document.querySelector(".projects-container").getBoundingClientRect()
-				.width / defaultcolumnWidth,
-		);
+    if (
+        numberOfColumns !=
+        Math.ceil(
+            document
+                .querySelector(".projects-container")
+                .getBoundingClientRect().width / defaultcolumnWidth,
+        )
+    ) {
+        numberOfColumns = Math.ceil(
+            document
+                .querySelector(".projects-container")
+                .getBoundingClientRect().width / defaultcolumnWidth,
+        );
 
-		document.querySelectorAll(".column").forEach((project) => {
-			project.remove();
-		});
+        document.querySelectorAll(".column").forEach((project) => {
+            project.remove();
+        });
 
-		loadProjects();
-		loadWebApps();
-		await loadRepositories();
-	}
+        loadProjects();
+        loadWebApps();
+        await loadRepositories();
+    }
 
-	let columns = document.querySelectorAll(".column");
+    let columns = document.querySelectorAll(".column");
 
-	columns.forEach((column) => {
-		column.style.width = `calc((${column.parentElement.getBoundingClientRect().width}px - ((${numberOfColumns} - 1) * 2rem)) / ${numberOfColumns})`;
-	});
+    columns.forEach((column) => {
+        column.style.width = `calc((${column.parentElement.getBoundingClientRect().width}px - ((${numberOfColumns} - 1) * 2rem)) / ${numberOfColumns})`;
+    });
 }
-
